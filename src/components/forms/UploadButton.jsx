3ce0affe2287ae29ac231a54875d5cf5b8db697c
DucSE170473 <DucNQSE170473@fpt.edu.vn>
2024-09-26 15:42:13 +0700

    update button Upload

--- src/components/forms/UploadButton.jsx
+++ src/components/forms/UploadButton.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-
+import "../../styles/UploadButton.css";
 const UploadButton = ({ onUpload, disabled }) => (
   <button
     onClick={onUpload}
     disabled={disabled}
-    className={`w-full px-4 py-2 font-semibold text-white rounded 
-                ${disabled ? "bg-blue-300 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}`}
+    className={`btn ${disabled ? "disabled" : ""}`}
   >
     Upload and View
   </button>
